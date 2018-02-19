07ee4a7e9bb65595e6156b1551de117164b849bc
Tomas Jovaišas <t.jovaisas@paysera.com>
2018-02-19 09:22:13 +0200

    Doc block whitespace fixer

---
+++ tests/Fixer/PhpBasic/CodeStyle/DocBlockWhitespaceFixerTest.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace Paysera\PhpCsFixerConfig\Tests\Fixer\PhpBasic\CodeStyle;
+
+use PhpCsFixer\Test\AbstractFixerTestCase;
+use Paysera\PhpCsFixerConfig\Fixer\PhpBasic\CodeStyle\DocBlockWhitespaceFixer;
+
+final class DocBlockWhitespaceFixerTest extends AbstractFixerTestCase
+{
+    /**
+     * @param string $expected
+     * @param null|string $input
+     *
+     * @dataProvider provideCases
+     */
+    public function testFix($expected, $input = null)
+    {
+        $this->doTest($expected, $input);
+    }
+
+    public function provideCases()
+    {
+        return [
+            [
+                '<?php
+namespace Some\Entity;
+class Sample
+{
+    /**
+     * @param $locationId
+     * @param $location
+     */
+    public function updateLocation($locationId, $location)
+    {
+    }
+}',
+                '<?php
+namespace Some\Entity;
+class Sample
+{
+    /**
+     * @param                                                      $locationId
+     * @param       $location
+     */
+    public function updateLocation($locationId, $location)
+    {
+    }
+}',
+            ],
+            [
+                '<?php
+namespace Some\Entity;
+class Sample
+{
+    /**
+     * @param $locationId
+     * @param Object|null $location
+     */
+    public function updateLocation($locationId, $location)
+    {
+    }
+}',
+                '<?php
+namespace Some\Entity;
+class Sample
+{
+    /**
+     * @param             $locationId
+     * @param Object|null $location
+     */
+    public function updateLocation($locationId, $location)
+    {
+    }
+}',
+            ],
+            [
+                '<?php
+namespace Some\Entity;
+class Sample
+{
+    /**
+     * @param Object|null $locationId
+     * @param Object $location
+     * @param $city
+     */
+    public function updateLocation($locationId, $location, $city)
+    {
+    }
+}',
+                '<?php
+namespace Some\Entity;
+class Sample
+{
+    /**
+     * @param Object|null $locationId
+     * @param Object      $location
+     * @param             $city
+     */
+    public function updateLocation($locationId, $location, $city)
+    {
+    }
+}',
+            ],
+        ];
+    }
+
+    public function createFixerFactory()
+    {
+        $fixerFactory = parent::createFixerFactory();
+        $fixerFactory->registerCustomFixers([
+            new DocBlockWhitespaceFixer(),
+        ]);
+        return $fixerFactory;
+    }
+
+    public function getFixerName()
+    {
+        return 'Paysera/php_basic_code_style_doc_block_whitespace';
+    }
+}
