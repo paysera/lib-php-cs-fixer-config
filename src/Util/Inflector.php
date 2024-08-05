4826375048697f4d22fc2c73fe1c8ca581c70180
Artem Yakymchuk <artem.yakymchuk@paysera.net>
2024-08-05 10:10:17 +0300

    CR changes

--- src/Util/Inflector.php
+++ src/Util/Inflector.php
@@ -20,12 +20,14 @@ class Inflector
         if (class_exists('Doctrine\Inflector\InflectorFactory')) {
             // Doctrine Inflector v2
             return InflectorFactory::create()->build();
-        } elseif (class_exists('Doctrine\Common\Inflector\Inflector')) {
-            // Doctrine Inflector v1
-           return new \Doctrine\Common\Inflector\Inflector();
-        } else {
-            throw new RuntimeException('Doctrine Inflector is not available');
         }
+
+        if (class_exists('Doctrine\Common\Inflector\Inflector')) {
+            // Doctrine Inflector v1
+            return new \Doctrine\Common\Inflector\Inflector();
+        }
+
+        throw new RuntimeException('Doctrine Inflector is not available');
     }
 
     public function pluralize($word)
@@ -33,10 +35,10 @@ class Inflector
         if (method_exists($this->inflector, 'pluralize')) {
             // Doctrine Inflector v2
             return $this->inflector->pluralize($word);
-        } else {
-            // Doctrine Inflector v1
-            return \Doctrine\Common\Inflector\Inflector::pluralize($word);
         }
+
+        // Doctrine Inflector v1
+        return \Doctrine\Common\Inflector\Inflector::pluralize($word);
     }
 
     public function singularize($word)
@@ -44,9 +46,9 @@ class Inflector
         if (method_exists($this->inflector, 'singularize')) {
             // Doctrine Inflector v2
             return $this->inflector->singularize($word);
-        } else {
-            // Doctrine Inflector v1
-            return \Doctrine\Common\Inflector\Inflector::singularize($word);
         }
+
+        // Doctrine Inflector v1
+        return \Doctrine\Common\Inflector\Inflector::singularize($word);
     }
 }
